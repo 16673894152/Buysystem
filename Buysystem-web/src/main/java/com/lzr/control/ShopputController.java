504f226b95f5c744043ee4c9f30dab3148b72d95
Xiaoquyao <1165426601@qq.com>
2020-12-21 11:50:51 +0800

    12.21 11:50

---
+++ Buysystem-web/src/main/java/com/lzr/control/ShopputController.java
@@ -0,0 +1,35 @@
+package com.lzr.control;
+
+import com.lzr.service.ShopputService;
+import com.lzr.vo.Shopput;
+import com.lzr.vo.Shoptype;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@CrossOrigin
+@RequestMapping("/shopput")
+public class ShopputController {
+
+    @Autowired
+    ShopputService shopputService;
+
+    @RequestMapping("/querydistilled.action")
+    @ResponseBody
+    public List<Shopput> queryDistilled() {
+        return shopputService.queryDistilled();
+    }
+
+    @RequestMapping("/querylike.action")
+    @ResponseBody
+    public List<Shopput> queryLike(Shopput shopput) {
+        System.out.println(shopput);
+        return shopputService.queryLike(shopput);
+    }
+
+}
